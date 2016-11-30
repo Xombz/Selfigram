4918093907d1923563b6b49e7a2d11ecaad868fd
Xombz <300149235@Administrators-Mac-mini-69.local>
2016-11-29 20:03:26 -0800

    added selfie cell

---
+++ Selfigram/SelfieCell.swift
@@ -0,0 +1,24 @@
+//
+//  Selfiecell.swift
+//  Selfigram
+//
+//  Created by Michael Belcher on 2016-11-29.
+//  Copyright © 2016 Liz Demer. All rights reserved.
+//
+
+import UIKit
+
+class Selfiecell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
